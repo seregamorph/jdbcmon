7606eaed7b8fa42b9c11fed660a247f8a8f35cff
seregamorph <serega.morph@gmail.com>
2016-11-13 16:20:34 +0300

    ConnectionProxy.ConnectionInvocationHandler

--- jdbcmon/src/main/java/org/jdbcmon/ConnectionProxy.java
+++ jdbcmon/src/main/java/org/jdbcmon/ConnectionProxy.java
@@ -1,6 +1,7 @@
 package org.jdbcmon;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -9,15 +10,23 @@ import java.sql.PreparedStatement;
 class ConnectionProxy {
     private static final Class[] CONNECTION_PROXY_INTERFACES = {Connection.class};
 
-    static Connection proxy(Connection delegate, SqlStat sqlStat) {
-        InvocationHandler handler = (proxy, method, args) -> {
+    private static class ConnectionInvocationHandler implements InvocationHandler {
+        private final Connection delegate;
+        private final SqlStat sqlStat;
+
+        private ConnectionInvocationHandler(Connection delegate, SqlStat sqlStat) {
+            this.delegate = delegate;
+            this.sqlStat = sqlStat;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             String methodName = method.getName();
             Throwable exception = null;
-            String sql = null;
 
             if (PreparedStatement.class.isAssignableFrom(method.getReturnType()) && "prepareStatement".equals(methodName)
                     && args.length > 0 && args[0] instanceof String) {
-                sql = (String) args[0];
+                String sql = (String) args[0];
                 try {
                     PreparedStatement delegatePS = (PreparedStatement) method.invoke(delegate, args);
                     return PreparedStatementProxy.proxy(sql, delegatePS, sqlStat);
@@ -27,25 +36,26 @@ class ConnectionProxy {
                 } finally {
                     sqlStat.registerPrepare(sql, exception);
                 }
-            } else {
-                if (CallableStatement.class.isAssignableFrom(method.getReturnType()) && "prepareCall".equals(methodName)
-                        && args.length > 0 && args[0] instanceof String) {
-                    sql = (String) args[0];
-                    try {
-                        CallableStatement delegateCS = (CallableStatement) method.invoke(delegate, args);
-                        return CallableStatementProxy.proxy(sql, delegateCS, sqlStat);
-                    } catch (Throwable e) {
-                        exception = e;
-                        throw e;
-                    } finally {
-                        sqlStat.registerPrepare(sql, exception);
-                    }
+            } else if (CallableStatement.class.isAssignableFrom(method.getReturnType()) && "prepareCall".equals(methodName)
+                    && args.length > 0 && args[0] instanceof String) {
+                String sql = (String) args[0];
+                try {
+                    CallableStatement delegateCS = (CallableStatement) method.invoke(delegate, args);
+                    return CallableStatementProxy.proxy(sql, delegateCS, sqlStat);
+                } catch (Throwable e) {
+                    exception = e;
+                    throw e;
+                } finally {
+                    sqlStat.registerPrepare(sql, exception);
                 }
             }
 
             return method.invoke(delegate, args);
-        };
+        }
+    }
 
+    static Connection proxy(Connection delegate, SqlStat sqlStat) {
+        InvocationHandler handler = new ConnectionInvocationHandler(delegate, sqlStat);
         return (Connection) Proxy.newProxyInstance(Connection.class.getClassLoader(), CONNECTION_PROXY_INTERFACES, handler);
     }
 }
